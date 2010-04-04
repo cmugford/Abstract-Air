abde80ed969ccf5db0d0f5243443f4f0a566c65e
danielok <dbokely@gmail.com>
2010-04-04 09:34:19 +0800

    Added an area - intellisense still working

---
+++ Example Web 2/Areas/Example2/Example2AreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Example_Web_2.Areas.Example2
+{
+    public class Example2AreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "Example2";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "Example2_default",
+                "Example2/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
