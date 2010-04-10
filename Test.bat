085b8dca0eb5859ab7078bd851e81151afa66bc7
Colin Scott <colin@abstractcode.com>
2010-04-10 13:52:16 +0800

    Change report directory and ignore. Remove paths to VS batch file in build files.

--- Test.bat
+++ Test.bat
@@ -1,2 +1,1 @@
-call "c:\Program Files\Microsoft Visual Studio 9.0\VC\vcvarsall.bat" x86
 %FrameworkDir%\%Framework35Version%\msbuild.exe /target:Test /property:Configuration=Debug "Abstract Air.build"
