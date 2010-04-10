29a193ea7335e0d13a1e2a111194e45294af81ca
danielok <dbokely@gmail.com>
2010-04-10 09:38:38 +0800

    Fixed project references

---
+++ GlobalAssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Abstract Air")]
+[assembly: AssemblyCopyright("Copyright © Colin David Scott 2010")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
